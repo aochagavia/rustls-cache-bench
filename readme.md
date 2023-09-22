4ca97a4d6363ee01719970588dfb4c2e55053e8c
Adolfo Ochagavía <github@adolfo.ochagavia.nl>
2023-09-22 12:15:34 +0200

    Remove batching, switch to mio

--- readme.md
+++ readme.md
@@ -1,7 +1,37 @@
 Rustls Cache Bench
 ==================
 
-See [this rustls issue](https://github.com/rustls/rustls/issues/1200) for details.
+See [this rustls issue](https://github.com/rustls/rustls/issues/1200) and the CLI `--help` for
+details.
 
-The benchmarks are under `src`. There is also `plots.py`, which I exported from a Jupyter Notebook I
-used to generate the plots linked in the issue.
+The benchmarks are under `src`.
+
+### Analyzing results
+
+For each benchmarked cache size, the following files are produced:
+
+#### `throughput.json`
+
+Example:
+
+```json
+{
+    "cache_size": 10000,
+    "handshakes_per_second": 4996.793827227934,
+    "server_saturation": 0.020581829251588035
+}
+```
+
+The `server_saturation` parameter is useful to know how much work the server threads where actually
+doing.
+
+#### `timings`
+
+Example:
+
+```
+182;197;197;212;194;193;199;199;...
+```
+
+This file contains a `;`-separated list with the duration of each request in microseconds. You can
+use it, for instance, to plot the latency distribution.
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::cell::{Cell, RefCell};
 use std::fs::File;
 use std::io::Write;
+use std::os::fd::AsRawFd;
 use std::path::{Path, PathBuf};
-use std::rc::Rc;
 use std::sync::Arc;
 use std::thread::JoinHandle;
 use std::time::{Duration, Instant};
 
 use clap::Parser;
+use crossbeam::channel::{Receiver, Sender, TrySendError};
 use fastrand::Rng;
+use mio::unix::SourceFd;
+use mio::{Events, Interest, Poll, Token};
 use rustls::server::{NoServerSessionStorage, ServerSessionMemoryCache, StoresServerSessions};
 use serde::Serialize;
-use tokio::sync::mpsc::{Receiver, Sender};
+use timerfd::{ClockId, SetTimeFlags, TimerFd, TimerState};
 
 /// The amount of tickets that are generated as part of a handshake
 const TICKETS_PER_HANDSHAKE: u64 = 4; // rustls' default
@@ -20,25 +22,30 @@ const TICKETS_PER_HANDSHAKE: u64 = 4; // rustls' default
 enum ClientMessage {
     /// Shutdown the server thread
     Shutdown,
-    /// Run the given amount of handshakes.
+    /// Simulate the execution of a handshake
     ///
     /// For each handshake, the server determines whether it should be full or resumed according to
     /// `Cli::resumed_handshake_ratio`.
-    HandshakeBatch(u64),
+    Handshake { request_id: usize },
 }
 
-/// A message from the server to the client, meaning that the handshake batch has completed
-struct ServerMessage;
+/// A message from the server to the client, meaning that the handshake has completed
+struct ServerMessage {
+    request_id: usize,
+}
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
-    /// The cache sizes to test (comma-separated)
+    /// The cache sizes to benchmark (comma-separated)
     #[arg(long, value_delimiter(','))]
     cache_sizes: Vec<usize>,
-    /// The total amount of "handshakes" that will be executed
-    #[arg(long, default_value("100000"))]
-    handshakes: u64,
+    /// The amount of handshakes to execute per second
+    #[arg(long, default_value("10000"))]
+    handshakes_per_second: u64,
+    /// The duration of each benchmark in seconds
+    #[arg(long, default_value("2"))]
+    duration_seconds: u64,
     /// The ratio of resumed handshakes per full handshake (between 0 and 1)
     #[arg(long, default_value("0.5"))]
     resumed_handshake_ratio: f64,
@@ -53,10 +60,6 @@ struct Cli {
     /// maximum possible throughput for the provided batch size)
     #[arg(long)]
     noop: bool,
-    /// The size of each handshake batch submitted by the client to the server's worker threads in a
-    /// single request (minimum value = 1)
-    #[arg(long, default_value("3"))]
-    batch_size: u64,
 }
 
 fn main() {
@@ -68,23 +71,21 @@ fn main() {
         println!("--resumed-handshake-ratio should be between 0 and 1 (run again with `--help` for details)");
         return;
     }
-    if cli.batch_size == 0 {
-        println!("--batch-size should be at least 1 (run again with `--help` for details)");
+    if cli.duration_seconds < 1 || cli.duration_seconds > 60 {
+        println!(
+            "--duration-seconds should be between 1 and 60 (run again with `--help` for details)"
+        );
         return;
     }
 
-    let rt = tokio::runtime::Builder::new_current_thread()
-        .build()
-        .unwrap();
-
     // Run benchmarks, once for each provided cache size
     for &cache_size in &cli.cache_sizes {
-        bench_run(&rt, cache_size, &cli);
+        bench_run(cache_size, &cli);
     }
 }
 
 /// Executes a single benchmark run, for a specific cache size
-fn bench_run(rt: &tokio::runtime::Runtime, cache_size: usize, cli: &Cli) {
+fn bench_run(cache_size: usize, cli: &Cli) {
     let server_threads = num_cpus::get_physical() - 1;
     if server_threads < 2 {
         panic!("These benchmarks only make sense on machines with at least 3 cores");
@@ -95,23 +96,101 @@ fn bench_run(rt: &tokio::runtime::Runtime, cache_size: usize, cli: &Cli) {
     } else {
         ServerSessionMemoryCache::new(cache_size)
     };
-
-    // Start the server
     let server = MultiThreadedServer::start(cache, server_threads, cli);
 
     // Hammer the server from our client!
-    let local = tokio::task::LocalSet::new();
-    let client_stats = local.block_on(
-        rt,
-        run_client(cli.handshakes, cli.batch_size, server.channels),
-    );
+    let mut poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(2);
 
-    // Shut down the server threads
+    let request_interval_micros = (1_000 * 1_000) / cli.handshakes_per_second;
+    let request_interval = interval(Duration::from_micros(request_interval_micros));
+    let request_fd = request_interval.as_raw_fd();
+    poll.registry()
+        .register(&mut SourceFd(&request_fd), Token(0), Interest::READABLE)
+        .unwrap();
+
+    let server_io_interval = interval(Duration::from_micros(10));
+    let server_io_fd = server_io_interval.as_raw_fd();
+    poll.registry()
+        .register(&mut SourceFd(&server_io_fd), Token(1), Interest::READABLE)
+        .unwrap();
+
+    let max_handshakes = max_handshakes(cli) as usize;
+    let mut request_start = Vec::with_capacity(max_handshakes);
+    let mut request_end = vec![None; max_handshakes];
+    let mut next_request_id = 0;
+    let mut concurrent_requests = 0;
+
+    let bench_start = Instant::now();
+    let finish = bench_start + Duration::from_secs(cli.duration_seconds);
+    loop {
+        poll.poll(&mut events, None).unwrap();
+        let handshakes_to_make = request_interval.read();
+        let do_server_io = server_io_interval.read() > 0;
+        let now = Instant::now();
+
+        if do_server_io {
+            // Actually enqueue started requests to the server (if the channel has capacity)
+            while next_request_id < request_start.len() {
+                match server.tx.try_send(ClientMessage::Handshake {
+                    request_id: next_request_id,
+                }) {
+                    Ok(_) => {
+                        next_request_id += 1;
+                        concurrent_requests += 1;
+                    }
+                    Err(TrySendError::Full(_)) => break,
+                    Err(TrySendError::Disconnected(_)) => unreachable!(),
+                }
+            }
+
+            // Receive responses from the server
+            while let Ok(msg) = server.rx.try_recv() {
+                request_end[msg.request_id] = Some(now);
+                concurrent_requests -= 1;
+            }
+        }
+
+        if now < finish {
+            // Start new requests
+            for _ in 0..handshakes_to_make {
+                request_start.push(now);
+            }
+        } else if concurrent_requests == 0 {
+            // We are done, shut down the servers
+            for _ in 0..server_threads {
+                server.tx.try_send(ClientMessage::Shutdown).unwrap();
+            }
+
+            break;
+        }
+    }
+
+    poll.registry()
+        .deregister(&mut SourceFd(&request_fd))
+        .unwrap();
+    poll.registry()
+        .deregister(&mut SourceFd(&server_io_fd))
+        .unwrap();
+
+    // Wait for the server threads to complete
     let server_stats = server
         .join_handles
         .into_iter()
         .map(|h| h.join().unwrap())
         .collect::<Vec<_>>();
+
+    // Gather client stats
+    let time_handshaking = Instant::now() - bench_start;
+    let mut client_durations = Vec::with_capacity(request_start.len());
+    for (start, end) in request_start.into_iter().zip(request_end) {
+        let end = end.unwrap();
+        client_durations.push(end - start);
+    }
+    let client_stats = ClientStats {
+        time_handshaking,
+        handshake_durations: client_durations,
+    };
 
     // Save to file for plotting
     save_handshake_stats(cache_size, &client_stats, &server_stats, &cli.output_dir);
@@ -140,88 +219,24 @@ fn save_handshake_stats(
         .sum();
 
     let total_handshakes: u64 = server_stats.iter().map(|s| s.handled_handshakes).sum();
-    let handshake_client_total = client_stats
-        .handshake_durations
-        .iter()
-        .sum::<Duration>();
-
+    let server_count = server_stats.len();
     let throughput_stats = ThroughputStats {
         cache_size,
-        handshakes_per_second: total_handshakes as f64 / client_stats.time_handshaking.as_secs_f64(),
-        server_saturation: handshake_server_total.as_secs_f64() / handshake_client_total.as_secs_f64(),
+        handshakes_per_second: total_handshakes as f64
+            / client_stats.time_handshaking.as_secs_f64(),
+        server_saturation: handshake_server_total.as_secs_f64()
+            / (client_stats.time_handshaking.as_secs_f64() * server_count as f64),
     };
 
     let file = File::create(base_dir.join("throughput.json")).unwrap();
     serde_json::to_writer(file, &throughput_stats).unwrap();
 }
 
-/// Hammers the server with as many requests as possible, from a single thread
-async fn run_client(
-    handshakes: u64,
-    batch_size: u64,
-    server_threads: Vec<(Sender<ClientMessage>, Receiver<ServerMessage>)>,
-) -> ClientStats {
-    let handshake_durations = Rc::new(RefCell::new(Vec::with_capacity(handshakes as usize)));
-    let handshakes_left = Rc::new(Cell::new(handshakes));
-
-    let start = Instant::now();
-
-    // Spawn a recurring handshaking task against each server thread
-    let mut handles = Vec::new();
-    for (tx, mut rx) in server_threads {
-        let handshake_durations = handshake_durations.clone();
-        let handshakes_left = handshakes_left.clone();
-
-        let handle = tokio::task::spawn_local(async move {
-            loop {
-                let left = handshakes_left.get();
-                if left == 0 {
-                    // No handshakes left, shutdown the server thread and finish the task
-                    tx.send(ClientMessage::Shutdown).await.unwrap();
-                    break;
-                } else {
-                    let batch_size = left.min(batch_size);
-                    handshakes_left.set(left - batch_size);
-
-                    let start_handshake = Instant::now();
-                    tx.send(ClientMessage::HandshakeBatch(batch_size))
-                        .await
-                        .unwrap();
-                    rx.recv().await.unwrap();
-                    let handshake_duration = Instant::now() - start_handshake;
-
-                    handshake_durations.borrow_mut().push(handshake_duration);
-                }
-            }
-        });
-
-        handles.push(handle);
-    }
-
-    // Wait until work for all threads has finished
-    for handle in handles {
-        handle.await.unwrap();
-    }
-
-    // This measurement is used to calculate throughput
-    let time_handshaking = Instant::now() - start;
-
-    // This measurement is used to plot a latency histogram and to calculate saturation (measures
-    // each request separately; subject to the overhead of exchanging data between the client and
-    // the server threads)
-    let handshake_durations = Rc::try_unwrap(handshake_durations).ok().unwrap();
-    let handshake_durations = handshake_durations.into_inner();
-
-    ClientStats {
-        time_handshaking,
-        handshake_durations,
-    }
-}
-
 /// A handshake server backed by multiple worker threads
 struct MultiThreadedServer {
     /// The channels that a client can use to communicate with each thread
-    channels: Vec<(Sender<ClientMessage>, Receiver<ServerMessage>)>,
+    tx: Sender<ClientMessage>,
+    rx: Receiver<ServerMessage>,
     /// The worker threads' join handles
     join_handles: Vec<JoinHandle<ServerStats>>,
 }
@@ -229,81 +244,81 @@ struct MultiThreadedServer {
 impl MultiThreadedServer {
     /// Start the server
     fn start(cache: Arc<dyn StoresServerSessions>, server_threads: usize, cli: &Cli) -> Self {
-        let mut channels = Vec::new();
         let mut join_handles = Vec::new();
+
+        let (tx, server_rx) = crossbeam::channel::bounded(server_threads * 8);
+        let (server_tx, rx) = crossbeam::channel::bounded(server_threads * 8);
 
         for i in 0..server_threads {
             let rng = Rng::with_seed(i as u64 * 42);
-            let (tx, rx, join_handle) = Self::start_thread(cache.clone(), rng, server_threads, cli);
-            channels.push((tx, rx));
+            let join_handle = Self::start_thread(
+                server_tx.clone(),
+                server_rx.clone(),
+                cache.clone(),
+                rng,
+                server_threads,
+                cli,
+            );
             join_handles.push(join_handle);
         }
 
         MultiThreadedServer {
-            channels,
+            tx,
+            rx,
             join_handles,
         }
     }
 
     /// Starts a single server thread
     fn start_thread(
+        server_tx: Sender<ServerMessage>,
+        server_rx: Receiver<ClientMessage>,
         cache: Arc<dyn StoresServerSessions>,
         mut rng: Rng,
         server_threads: usize,
         cli: &Cli,
-    ) -> (
-        Sender<ClientMessage>,
-        Receiver<ServerMessage>,
-        JoinHandle<ServerStats>,
-    ) {
-        let (server_tx, client_rx) = tokio::sync::mpsc::channel(1);
-        let (client_tx, mut server_rx) = tokio::sync::mpsc::channel(1);
-
+    ) -> JoinHandle<ServerStats> {
         // Keep a ticket around for resumed handshakes
         let mut ticket = generate_ticket(&mut rng);
         cache.put(ticket.clone(), generate_value());
 
         let resumed_handshake_ratio = cli.resumed_handshake_ratio;
-        let mut stats = ServerStats::with_enough_capacity(server_threads, cli.handshakes);
-        let handle = std::thread::spawn(move || {
+        let mut stats = ServerStats::with_enough_capacity(server_threads, max_handshakes(cli));
+        std::thread::spawn(move || {
             loop {
-                let request = server_rx.blocking_recv().unwrap();
-                let batch_size = match request {
+                let request = server_rx.recv().unwrap();
+                let request_id = match request {
                     ClientMessage::Shutdown => break,
-                    ClientMessage::HandshakeBatch(batch_size) => batch_size,
+                    ClientMessage::Handshake { request_id } => request_id,
                 };
 
                 let start = Instant::now();
 
-                for _ in 0..batch_size {
-                    let should_resume = rng.f64() < resumed_handshake_ratio;
-                    if should_resume {
-                        // Resumed handshakes call `ServerSessionMemoryCache::take` (in TLS 1.3)
-                        cache.take(&ticket);
-                    }
-
-                    // Generate the same amount of new tickets as rustls (though without a
-                    // cryptographically secure generator)
-                    let mut tickets = Vec::with_capacity(TICKETS_PER_HANDSHAKE as usize);
-                    for _ in 0..TICKETS_PER_HANDSHAKE {
-                        let key = generate_ticket(&mut rng);
-                        tickets.push(key.clone());
-                        cache.put(key, generate_value());
-                    }
-
-                    ticket = tickets.into_iter().next().unwrap();
+                let should_resume = rng.f64() < resumed_handshake_ratio;
+                if should_resume {
+                    // Resumed handshakes call `ServerSessionMemoryCache::take` (in TLS 1.3)
+                    cache.take(&ticket);
                 }
 
+                // Generate the same amount of new tickets as rustls (though without a
+                // cryptographically secure generator)
+                let mut tickets = Vec::with_capacity(TICKETS_PER_HANDSHAKE as usize);
+                for _ in 0..TICKETS_PER_HANDSHAKE {
+                    let key = generate_ticket(&mut rng);
+                    tickets.push(key.clone());
+                    cache.put(key, generate_value());
+                }
+
+                ticket = tickets.into_iter().next().unwrap();
+
                 let done = Instant::now();
-                stats.handled_handshakes += batch_size;
+                stats.handled_handshakes += 1;
                 stats.handshake_durations.push(done - start);
-                server_tx.blocking_send(ServerMessage).unwrap();
+                server_tx.send(ServerMessage { request_id }).unwrap();
             }
 
             stats
-        });
-
-        (client_tx, client_rx, handle)
+        })
     }
 }
 
@@ -347,4 +362,20 @@ impl ServerStats {
             handshake_durations,
         }
     }
+}
+
+fn interval(duration: Duration) -> TimerFd {
+    let mut tfd = TimerFd::new_custom(ClockId::Monotonic, true, false).unwrap();
+    tfd.set_state(
+        TimerState::Periodic {
+            current: duration,
+            interval: duration,
+        },
+        SetTimeFlags::Default,
+    );
+    tfd
+}
+
+fn max_handshakes(cli: &Cli) -> u64 {
+    cli.handshakes_per_second * cli.duration_seconds + 100
 }
